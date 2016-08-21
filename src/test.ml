fa40524571611fe70605c9e318bc110b0d61c6aa
laurent <laurent.mazare@gmail.com>
2016-08-21 21:22:42 +0100

    Proper testing.

--- src/test.ml
+++ src/test.ml
@@ -1,36 +1,102 @@
-let print_array2 array2 to_string =
-  let dim1 = Bigarray.Array2.dim1 array2 in
-  let dim2 = Bigarray.Array2.dim2 array2 in
-  for idx1 = 0 to dim1 - 1 do
-    for idx2 = 0 to dim2 - 1 do
-      Printf.printf "%d %d => %s\n" idx1 idx2 (to_string array2.{idx1, idx2})
-    done;
-  done
+let verbose = false
 
-let save_dummy_array filename =
-  let dim1 = 2 in
-  let dim2 = 3 in
+let md5sum filename =
+  let in_channel =
+    Unix.open_process_in
+      (Printf.sprintf "md5sum %s | cut -d ' ' -f 1" filename)
+  in
+  let md5 = input_line in_channel in
+  assert (Unix.close_process_in in_channel = WEXITED 0);
+  md5
+
+(* Saves a npy file and reread it checking that the read content is identical. *)
+let save_and_read (type a b) (array : (a, b, Bigarray.c_layout) Bigarray.Genarray.t) filename =
+  Npy.write array filename;
+  let Npy.P rarray = Npy.read_mmap filename ~shared:false in
+  begin
+    match Bigarray.Genarray.layout rarray with
+    | Bigarray.Fortran_layout -> assert false
+    | Bigarray.C_layout ->
+      match Bigarray.Genarray.kind array, Bigarray.Genarray.kind rarray with
+      | Bigarray.Float32, Bigarray.Float32 -> assert (array = rarray)
+      | Bigarray.Float64, Bigarray.Float64 -> assert (array = rarray)
+      | Bigarray.Int32, Bigarray.Int32 -> assert (array = rarray)
+      | Bigarray.Int64, Bigarray.Int64 -> assert (array = rarray)
+      | _ -> assert false
+  end
+
+(* Save a npy file using python and numpy. *)
+let save_with_python (type a b) (array : (a, b, Bigarray.c_layout) Bigarray.Genarray.t) filename =
+  let run array to_string dtype =
+    let rec to_string_loop dim idxs =
+      if dim = Bigarray.Genarray.num_dims array
+      then Bigarray.Genarray.get array (List.rev idxs |> Array.of_list) |> to_string
+      else
+        Array.init
+          (Bigarray.Genarray.nth_dim array dim)
+          (fun i -> to_string_loop (dim + 1) (i :: idxs))
+        |> Array.to_list
+        |> String.concat ", "
+        |> Printf.sprintf "[ %s ]"
+    in
+    let cmd =
+      Printf.sprintf
+        "python -c 'import numpy as np\n\
+                    arr = np.array(%s)\n\
+                    np.save(\"%s\", arr.astype(\"%s\"))'"
+        (to_string_loop 0 [])
+        filename
+        dtype
+    in
+    if verbose
+    then Printf.printf "Running: %s\n\n%!" cmd;
+    assert (Unix.system cmd = WEXITED 0)
+  in
+  match Bigarray.Genarray.kind array with
+  | Bigarray.Float32 -> run array string_of_float "f4"
+  | Bigarray.Float64 -> run array string_of_float "f8"
+  | Bigarray.Int32 -> run array Int32.to_string "i4"
+  | Bigarray.Int64 -> run array Int64.to_string "i8"
+  | _ -> assert false
+
+(* - Save a npy file using the library.
+   - Load the npy file, check that the content is identical to the input.
+   - Save the npy file using python and numpy.
+   - Check that the two npy files have the same md5 sum.
+*)
+let run_test (type a b) (array : (a, b, Bigarray.c_layout) Bigarray.Genarray.t) filename =
+  save_and_read array filename;
+  let md5 = md5sum filename in
+  let python_filename = "p" ^ filename in
+  save_with_python array python_filename;
+  let md5p = md5sum python_filename in
+  if verbose
+  then Printf.printf "%s %s %s\n%!" filename md5 md5p;
+  assert (md5 = md5p)
+
+let array2_test (type a) (kind : (a, _) Bigarray.kind) (random : unit -> a) filename ~dim1 ~dim2 =
   let bigarray =
     Bigarray.Array2.create
-      Float32
+      kind
       C_layout
       dim1
       dim2
   in
   for idx1 = 0 to dim1 - 1 do
     for idx2 = 0 to dim2 - 1 do
-      Bigarray.Array2.set bigarray idx1 idx2 (idx1 + idx2 * dim1 |> float)
+      Bigarray.Array2.set bigarray idx1 idx2 (random ())
     done;
   done;
-  Npy.write2 bigarray filename;
-  let Npy.P array2 = Npy.read_mmap filename ~shared:false in
-  let array2 = Bigarray.array2_of_genarray array2 in
-  match Bigarray.Array2.kind array2 with
-  | Bigarray.Float32 -> print_array2 array2 string_of_float
-  | Bigarray.Float64 -> print_array2 array2 string_of_float
-  | Bigarray.Int32 -> print_array2 array2 Int32.to_string
-  | Bigarray.Int64 -> print_array2 array2 Int64.to_string
-  | _ -> assert false
+  run_test (Bigarray.genarray_of_array2 bigarray) filename
 
 let () =
-  save_dummy_array "test.npy"
+  Random.init 42;
+  let random_float () = Random.float 1e9 |> floor in
+  let random_int32 () = Int32.sub (Random.int32 2_000_000l) 1_000_000l in
+  let random_int64 () =
+    Int64.sub (Random.int64 2_000_000_000_000_000L) 1_000_000_000_000_000L
+  in
+  array2_test Float32 random_float "test1.npy" ~dim1:2 ~dim2:3;
+  array2_test Float64 random_float "test2.npy" ~dim1:8 ~dim2:21;
+  array2_test Int32 random_int32 "test3.npy" ~dim1:7 ~dim2:4;
+  array2_test Int64 random_int64 "test4.npy" ~dim1:8 ~dim2:21;
