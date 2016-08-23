0d71c1ed41ca7a4b550297a96e7b6c8eb9a8cba9
laurent <laurent.mazare@gmail.com>
2016-08-23 21:59:58 +0100

    More opam support.

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
+npy.lib: .FORCE
+	ocamlbuild npy.cmxa npy.cma npy.cmxs npy.cmx
+
 test:
 	ocamlbuild tests/test.native
 	./test.native
 
 clean:
 	rm -Rf _build *.native *.npy
+
+.FORCE:
--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,6 +1,8 @@
 open Ocamlbuild_plugin
 let () =
   dispatch (function
+    | Before_options ->
+      Options.use_ocamlfind := true
     | After_rules ->
       ocaml_lib "src/npy"
     | _ -> ())
